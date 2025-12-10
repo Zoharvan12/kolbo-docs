1ca7dabd17928603f63077d1c649c40ee7fd505f
Zoharvan12 <91822245+Zoharvan12@users.noreply.github.com>
2025-12-10 18:10:26 +0200

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,11 +58,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${inter.className} ${poppins.variable}`} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider
-          search={{
-            enabled: true,
-          }}
-        >
+        <RootProvider>
           {children}
         </RootProvider>
       </body>
